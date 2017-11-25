fa77907a316fee9174baf325c07a8c7c0d5df4d7
Sanket  Singh <sanketsingh@Sankets-MacBook-Air.local>
2017-11-25 10:08:55 +0530

    Index restful route setup

---
+++ app.js
@@ -0,0 +1,41 @@
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+
+//APP CONGIG
+app.set("view engine","ejs");
+mongoose.connect("mongodb://localhost/restful_blog_app");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+// MONGOOSE/MODEL/CONFIG
+var blogSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    body: String,
+    created: {type: Date, default: Date.now()}
+});
+var Blog = mongoose.model("Blog",blogSchema);
+
+//RESTFUL ROUTES
+app.get("/",function (req,res) {
+    res.redirect("/blogs");
+});
+app.get("/blogs",function (req,res) {
+    Blog.find({},function (err,blogs) {
+        if(err){
+            console.log(err);
+        } else {
+            res.render("index",{blogs: blogs});
+        }
+    })
+});
+
+
+
+app.listen(3000,function () {
+    console.log("Server is running");
+});
+
