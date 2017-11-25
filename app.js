dc30e4f44e02197d15314bab4e45a9d8f66682b8
Sanket  Singh <sanketsingh@Sankets-MacBook-Air.local>
2017-11-25 11:57:31 +0530

    New and create added

--- app.js
+++ app.js
@@ -32,7 +32,18 @@ app.get("/blogs",function (req,res) {
         }
     })
 });
-
+app.get("/blogs/new",function (req,res) {
+    res.render("new");
+});
+app.post("/blogs",function (req,res) {
+    Blog.create(req.body.blog,function (err,newBlog) {
+        if(err){
+            res.render("new");
+        } else {
+            res.redirect("/blogs");
+        }
+    });
+});
 
 
 app.listen(3000,function () {
